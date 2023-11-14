2f7df809671fbf9a6b39e22743080aee71810064
SumeetSanwal <63290497+SumeetSanwal@users.noreply.github.com>
2023-11-14 11:47:22 +0530

    Fixed formatting

--- mass_accept.py
+++ mass_accept.py
@@ -7,51 +7,54 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 
+def mass_accept():
+    #Detach Chrome Window, and not auto close when code finishes.
+    options = Options()
+    options.add_experimental_option("detach", True)
+    driver = webdriver.Chrome(options=options)
+    wait = WebDriverWait(driver, 200)
 
-#Detach Chrome Window, and not auto close when code finishes.
-options = Options()
-options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(options=options)
-wait = WebDriverWait(driver, 200)
-
-#Open Sign in page
-driver.get("https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo/")
-wait = WebDriverWait(driver, 800)
-time.sleep(50) #Time for user to login
-
-# Send login id & password
-# driver.find_element(By.ID, "session_key").send_keys("abc@gmail.com")
-# driver.find_element(By.ID, "session_password").send_keys("XXXX")
-# driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
-# time.sleep(50)
-
-
-
-
-i=0 #Page count
-j=0 #Total Accepted request count
-while i<25: #Number of pages = Number of request / 100
-#Open the following page
-    driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.COMMAND + 't')
-    driver.get("https://www.linkedin.com/mynetwork/invitation-manager/?filterCriteria=&invitationType=&page=1")
+    #Open Sign in page
+    driver.get("https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo/")
     wait = WebDriverWait(driver, 800)
-    i+=1
-    time.sleep(5)
+    time.sleep(50) #Time for user to login
 
-    #Get all accept buttons list
-    wait = WebDriverWait(driver, 800)
-    ele_list = driver.find_elements(By.XPATH,"//button[contains(.,'Accept')]")
+    # Send login id & password
+    # driver.find_element(By.ID, "session_key").send_keys("abc@gmail.com")
+    # driver.find_element(By.ID, "session_password").send_keys("XXXX")
+    # driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+    # time.sleep(50)
 
-    for ele in ele_list:
-        #Scroll to the element in focus
+
+
+
+    i=0 #Page count
+    j=0 #Total Accepted request count
+    while i<25: #Number of pages = Number of request / 100
+    #Open the following page
+        driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.COMMAND + 't')
+        driver.get("https://www.linkedin.com/mynetwork/invitation-manager/?filterCriteria=&invitationType=&page=1")
         wait = WebDriverWait(driver, 800)
-        j+=1
-        iframe = driver.find_element(By.ID, ele.get_attribute("id"))
-        ActionChains(driver).scroll_to_element(iframe).perform()
-        time.sleep(1)
-        #Click on accept button
-        t1_button = driver.find_element(By.ID, ele.get_attribute("id"))
-        driver.execute_script("arguments[0].click();", t1_button)
-        time.sleep(1)
+        i+=1
+        time.sleep(5)
 
-    print(f"Number of connections accepted till now: {j}")
+        #Get all accept buttons list
+        wait = WebDriverWait(driver, 800)
+        ele_list = driver.find_elements(By.XPATH,"//button[contains(.,'Accept')]")
+
+        for ele in ele_list:
+            #Scroll to the element in focus
+            wait = WebDriverWait(driver, 800)
+            j+=1
+            iframe = driver.find_element(By.ID, ele.get_attribute("id"))
+            ActionChains(driver).scroll_to_element(iframe).perform()
+            time.sleep(1)
+            #Click on accept button
+            t1_button = driver.find_element(By.ID, ele.get_attribute("id"))
+            driver.execute_script("arguments[0].click();", t1_button)
+            time.sleep(1)
+
+        print(f"Number of connections accepted till now: {j}")
+
+if __name__ == "__main__":
+    mass_accept()
--- mass_unfollow.py
+++ mass_unfollow.py
@@ -7,17 +7,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 
-if __name__ == '__main__':
-    #Detach Chrome Window, and not auto close when code finishes.
+def mass_unfollow():
+    # Detach Chrome Window, and not auto close when code finishes.
     options = Options()
     options.add_experimental_option("detach", True)
     driver = webdriver.Chrome(options=options)
     wait = WebDriverWait(driver, 200)
 
-    #Open Sign in page
+    # Open Sign in page
     driver.get("https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo/")
     wait = WebDriverWait(driver, 800)
-    time.sleep(150) #Time for user to login
+    time.sleep(150)  # Time for user to login
 
     # Send login id & password
     # driver.find_element(By.ID, "session_key").send_keys("abc@gmail.com")
@@ -25,15 +25,14 @@ if __name__ == '__main__':
     # driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
     # time.sleep(50)
 
-    #Open New TAB
+    # Open New TAB
     driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.COMMAND + 't')
 
-    #Open the following page
+    # Open the following page
     driver.get("https://www.linkedin.com/mynetwork/network-manager/people-follow/followers/")
     wait = WebDriverWait(driver, 800)
 
-
-    #Scroll till bottom
+    # Scroll till bottom
 
     # Get scroll height
     last_height = driver.execute_script("return document.body.scrollHeight")
@@ -50,35 +49,39 @@ if __name__ == '__main__':
             break
         last_height = new_height
 
-
-
-    #Get all following buttons list
+    # Get all following buttons list
     wait = WebDriverWait(driver, 800)
-    ele_list = driver.find_elements(By.XPATH,"//button[contains(.,'Following')]")
+    ele_list = driver.find_elements(By.XPATH, "//button[contains(.,'Following')]")
 
-    #Pop the following tab element
+    # Pop the following tab element
     ele_list.pop(0)
     l = len(ele_list)
 
     print(f"Number of connections following : {l}")
 
-    i=1 #Count for number for folks unfollowed
+    i = 1  # Count for number for folks unfollowed
     for ele in ele_list:
-        #Scroll to the element in focus
+        # Scroll to the element in focus
         wait = WebDriverWait(driver, 800)
-        print(str(i)+ '. ' + ele.get_attribute("aria-label")) #Unfollowed person details
-        i+=1
+        print(str(i) + '. ' + ele.get_attribute("aria-label"))  # Unfollowed person details
+        i += 1
         iframe = driver.find_element(By.ID, ele.get_attribute("id"))
         ActionChains(driver).scroll_to_element(iframe).perform()
         time.sleep(1)
-        #Click on following button
+        # Click on following button
         t1_button = driver.find_element(By.ID, ele.get_attribute("id"))
         driver.execute_script("arguments[0].click();", t1_button)
         time.sleep(1)
-        #Click on unfollow button
+        # Click on unfollow button
         wait = WebDriverWait(driver, 1000)
         button = driver.find_element(By.XPATH, "//button[contains(.,'Unfollow')]")
         driver.execute_script("arguments[0].click();", button)
         time.sleep(1)
 
     print(f"Number of connections unfollowed : {i}")
+
+
+
+
+if __name__ == '__main__':
+   mass_unfollow()
